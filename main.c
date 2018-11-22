e2ac73c4850a38dfcafa52f69f0c5c10ce2c2cfa
dbwpdls22 <dbwpdls22@naver.com>
2018-11-22 15:53:05 +0900

    12_4

--- main.c
+++ main.c
@@ -6,16 +6,25 @@
 
 int main(int argc, char *argv[]) {
 	FILE *fp = NULL;
-	char input;
+	//char input;
+	char input[100];
 	
 	
 	fp=fopen("sample.txt","r");
-	
+	/*
 	while((input = fgetc(fp)) != EOF)
 	{
 		putchar(input);
 	}
+	*/
 	
+	
+	while(fgets(input, 100 ,fp) != NULL)
+	{
+	    printf(input);
+    }
+    
+    
 	fclose(fp);
 	
 	return 0;
