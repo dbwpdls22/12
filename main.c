35ef0f05ac253e1fc3da1226d845d037ccb71a0d
dbwpdls22 <dbwpdls22@naver.com>
2018-11-22 15:21:18 +0900

    12_2

--- main.c
+++ main.c
@@ -5,17 +5,16 @@
 
 int main(int argc, char *argv[]) {
 	char src[100]="Programming course";
-	char dst[100];
-	int i;
+	int i=0;
 	
 	while(src[i]!=NULL)
 	{
-		dst[i]=src[i];
 		i++;
 	}
-	dst[i]='\0';
 	
-	printf("%s\n",dst);
+	
+	printf("문자열 %s의 길이:%i \n",src,i);
 	
 	return 0;
+	
 }
